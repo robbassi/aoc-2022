fc49040c606d0e079a254ff949a3b5b06050f85b
Rob Bassi <rob.bassi.89@gmail.com>
2022-12-03 23:19:42 -0500

    Refactoring

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,21 +2,24 @@ use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 
+fn char_to_score(c: char) -> i32 {
+    if c.is_ascii_lowercase() {
+        c as i32 - 96
+    } else {
+        c as i32 - 64 + 26
+    }
+}
+
 fn part1(input: &Vec<String>) -> i32 {
     input.into_iter().fold(0, |priorities, line| {
         let chars = line.as_str().chars();
-        let common: char = {
+        let common = {
             let half = line.len() / 2;
-            let comp1: HashSet<char> = chars.clone().take(half).collect();
-            let comp2: HashSet<char> = chars.skip(half).collect();
-            let common = comp1.intersection(&comp2).nth(0).unwrap();
-            *common
+            let comp1: HashSet<_> = chars.clone().take(half).collect();
+            let comp2: HashSet<_> = chars.skip(half).collect();
+            *comp1.intersection(&comp2).nth(0).unwrap()
         };
-        if common.is_ascii_lowercase() {
-            priorities + common as i32 - 96
-        } else {
-            priorities + common as i32 - 64 + 26
-        }
+        priorities + char_to_score(common)
     })
 }
 
@@ -24,20 +27,15 @@ fn part2(input: &Vec<String>) -> i32 {
     let mut i = 0;
     let mut priorities = 0;
     while i < input.len() {
-        let elf1: HashSet<char> = input[i].as_str().chars().collect();
-        let elf2: HashSet<char> = input[i + 1].as_str().chars().collect();
-        let elf3: HashSet<char> = input[i + 2].as_str().chars().collect();
-        let common: char = *elf1
-            .intersection(&elf2)
-            .copied()
-            .collect::<HashSet<_>>()
-            .intersection(&elf3)
-            .nth(0)
-            .unwrap();
-        if common.is_ascii_lowercase() {
-            priorities += common as i32 - 96;
-        } else {
-            priorities += common as i32 - 64 + 26;
+        let elf1: HashSet<_> = input[i].as_str().chars().collect();
+        let elf2: HashSet<_> = input[i + 1].as_str().chars().collect();
+        let common: HashSet<_> = elf1.intersection(&elf2).copied().collect();
+        // we don't have to build a set for elf3
+        for c in input[i + 2].as_str().chars() {
+            if common.contains(&c) {
+                priorities += char_to_score(c);
+                break;
+            }
         }
         i += 3;
     }

6b604479c0153ed4d645d783ee5c9490dffe07e4
Rob Bassi <rob.bassi.89@gmail.com>
2022-12-03 23:33:35 -0500

    Add trait for char priority

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,24 +2,30 @@ use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 
-fn char_to_score(c: char) -> i32 {
-    if c.is_ascii_lowercase() {
-        c as i32 - 96
-    } else {
-        c as i32 - 64 + 26
+pub trait Priority {
+    fn priority(&self) -> i32;
+}
+
+impl Priority for char {
+    fn priority(&self) -> i32 {
+        if self.is_ascii_lowercase() {
+            *self as i32 - 96
+        } else {
+            *self as i32 - 64 + 26
+        }
     }
 }
 
 fn part1(input: &Vec<String>) -> i32 {
     input.into_iter().fold(0, |priorities, line| {
         let chars = line.as_str().chars();
-        let common = {
+        let common_item = {
             let half = line.len() / 2;
             let comp1: HashSet<_> = chars.clone().take(half).collect();
             let comp2: HashSet<_> = chars.skip(half).collect();
             *comp1.intersection(&comp2).nth(0).unwrap()
         };
-        priorities + char_to_score(common)
+        priorities + common_item.priority()
     })
 }
 
@@ -29,11 +35,11 @@ fn part2(input: &Vec<String>) -> i32 {
     while i < input.len() {
         let elf1: HashSet<_> = input[i].as_str().chars().collect();
         let elf2: HashSet<_> = input[i + 1].as_str().chars().collect();
-        let common: HashSet<_> = elf1.intersection(&elf2).copied().collect();
+        let common_items: HashSet<_> = elf1.intersection(&elf2).copied().collect();
         // we don't have to build a set for elf3
-        for c in input[i + 2].as_str().chars() {
-            if common.contains(&c) {
-                priorities += char_to_score(c);
+        for item in input[i + 2].as_str().chars() {
+            if common_items.contains(&item) {
+                priorities += item.priority();
                 break;
             }
         }

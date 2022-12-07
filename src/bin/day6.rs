1469142fad8651a986c573535b52e29daeea9449
Rob Bassi <rob.bassi.89@gmail.com>
2022-12-06 21:36:21 -0500

    Day 6 - optimizations

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::io;
 use std::io::BufRead;
 
@@ -13,8 +14,42 @@ fn first_unique_window(input: &String, n: usize) -> Option<usize> {
     None
 }
 
+fn first_unique_window_linear(input: &String, n: usize) -> Option<usize> {
+    let mut windows = input.as_bytes().windows(n);
+    let first = windows.next().unwrap();
+    let mut queue: VecDeque<u8> = first.iter().copied().collect();
+    let mut unique: HashMap<u8, i32> = HashMap::new();
+    // build frequencies
+    for x in queue.iter() {
+        *unique.entry(*x).or_insert(0) += 1;
+    }
+    if unique.len() == n {
+        return Some(n);
+    }
+    let mut indexed_windows = windows.enumerate();
+    while let Some((i, window)) = indexed_windows.next() {
+        let last = queue.pop_front().unwrap();
+        // decrement
+        unique.entry(last).and_modify(|x| *x -= 1);
+        // remove if last
+        if unique[&last] == 0 {
+            unique.remove(&last);
+        }
+        // insert or increment the next value
+        *unique.entry(window[n - 1]).or_insert(0) += 1;
+        queue.push_back(window[n - 1]);
+        if unique.len() == n {
+            return Some(i + n + 1);
+        }
+    }
+    None
+}
+
 fn main() {
     let input: Vec<String> = io::stdin().lock().lines().map(Result::unwrap).collect();
     println!("part 1 = {}", first_unique_window(&input[0], 4).unwrap());
-    println!("part 2 = {}", first_unique_window(&input[0], 14).unwrap());
+    println!(
+        "part 2 = {}",
+        first_unique_window_linear(&input[0], 14).unwrap()
+    );
 }
